db820df92722e4400178d323a53dc76e75e5fb1a
windows-ihor <ig.mytlushko@gmail.com>
2022-02-19 17:43:26 +0200

    changes

--- main.tf
+++ main.tf
@@ -10,6 +10,73 @@ resource "aws_db_instance" "gitbucket_db" {
   skip_final_snapshot  = true
 }
 
+resource "aws_codepipeline" "codepipeline" {
+  name     = "tf-test-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  } 
+
+  stage {
+      name = "Source"
+
+      action {
+        name             = "Source"
+        category         = "Source"
+        owner            = "AWS"
+        provider         = "CodeStarSourceConnection"
+        version          = "1"
+        output_artifacts = ["source_output"]
+
+        configuration = {
+          ConnectionArn    = aws_codestarconnections_connection.example.arn
+          FullRepositoryId = "my-organization/example"
+          BranchName       = "main"
+        }
+      }
+  }
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = "test"
+      }
+    }
+  }
+stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CloudFormation"
+      input_artifacts = ["build_output"]
+      version         = "1"
+
+      configuration = {
+        ActionMode     = "REPLACE_ON_FAILURE"
+        Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
+        OutputFileName = "CreateStackOutput.json"
+        StackName      = "MyStack"
+        TemplatePath   = "build_output::sam-templated.yaml"
+          } 
+        } 
+      }
+}
+
+#GITBUCKET_BASE_URL=http://
 
 resource "aws_codebuild_project" "example" {
   name          = "gitbucket_build"
@@ -64,4 +131,11 @@ resource "aws_elastic_beanstalk_configuration_environment" "eb-env" {
   name                = "eb-env-config"
   application         = aws_elastic_beanstalk_application.eb-app.name
   solution_stack_name = "64bit Amazon Linux 2 v4.2.11 running Tomcat 8.5 Corretto 11"
+  setting {
+    namespace = "aws:ec2:instances"
+    name = "InstanceTypes"
+    value = "t2.micro"
+  } 
 }
+
+
